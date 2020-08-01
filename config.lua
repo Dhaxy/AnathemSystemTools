c0677713945041bad51b6faa7feff2b58db375e1
Arsene <aflament68@live.fr>
2020-08-01 16:36:55 +0200

    Ajout des tabs

--- config.lua
+++ config.lua
@@ -36,13 +36,17 @@ function Config:GetThemeColor()
 end
 
 function Config:CreateButton(point, relativeFrame, relativePoint, yOffset, text)
-    local button = CreateFrame("Button", nil, UICharacterSheet.ScrollFrame, "GameMenuButtonTemplate");
-   button:SetPoint(point, relativeFrame, relativePoint, 0, yOffset);
-   button:SetSize(80, 40);
-   button:SetText(text);
-   button:SetNormalFontObject("GameFontNormalLarge");
-   button:SetHighlightFontObject("GameFontHighlightLarge");
-   return button; 
+    local button = CreateFrame("Button", nil, relativeFrame, "GameMenuButtonTemplate");
+    button:SetPoint(point, relativeFrame, relativePoint, 0, yOffset);
+    button:SetSize(80, 40);
+    button:SetText(text);
+    button:SetNormalFontObject("GameFontNormalLarge");
+    button:SetHighlightFontObject("GameFontHighlightLarge");
+    return button; 
+end
+
+function Config:CreateMinimapButton(point, relativeFrame, relativePoint, yOffset)
+    local button = CreateFrame("Button", nil, relativeFrame, "");
 end
 
 local function ScrollFrame_OnMouseWheel(self, delta)
@@ -57,6 +61,47 @@ local function ScrollFrame_OnMouseWheel(self, delta)
 	self:SetVerticalScroll(newValue);
 end
 
+local function Tab_OnClick(self)
+    PanelTemplates_SetTab(self:GetParent(), self:GetID());
+
+    local scrollChild = UICharacterSheet.ScrollFrame:GetScrollChild();
+    if (scrollChild) then
+        scrollChild:Hide();
+    end
+
+    UICharacterSheet.ScrollFrame:SetScrollChild(self.content);
+    self.content:Show();
+end
+
+local function SetTabs(frame, numTabs, ...)
+    frame.numTabs = numTabs;
+    local contents = {};
+    local frameName = frame:GetName();
+
+    for i = 1, numTabs do
+        local tab = CreateFrame("Button", frameName.."Tab"..i, frame, "CharacterFrameTabButtonTemplate");
+        tab:SetID(i);
+        tab:SetText(select(i, ...));
+        tab:SetScript("OnClick", Tab_OnClick);
+
+        tab.content = CreateFrame("Frame", nil, UICharacterSheet.ScrollFrame);
+        tab.content:SetSize(520, 540);
+        tab.content:Hide();
+
+        table.insert(contents, tab.content);
+
+        if (i == 1) then
+            tab:SetPoint("TOPLEFT", UICharacterSheet, "BOTTOMLEFT", 5, 2);
+        else
+            tab:SetPoint("TOPLEFT", _G[frameName.."Tab"..(i - 1)], "TOPRIGHT", -14, 0);
+        end
+    end
+
+    Tab_OnClick(_G[frameName.."Tab1"]);
+
+    return unpack(contents);
+end
+
 function Config:CreateMenu()
 
     ----------------------------------
@@ -66,6 +111,11 @@ function Config:CreateMenu()
     UICharacterSheet = CreateFrame("Frame", "AnathemCharacterSheet", UIParent, "BasicFrameTemplate");
     UICharacterSheet:SetSize(550, 600);
     UICharacterSheet:SetPoint("CENTER");
+    UICharacterSheet:SetMovable(true);
+    UICharacterSheet:EnableMouse(true);
+    UICharacterSheet:RegisterForDrag("LeftButton");
+    UICharacterSheet:SetScript("OnDragStart", UICharacterSheet.StartMoving);
+    UICharacterSheet:SetScript("OnDragStop", UICharacterSheet.StopMovingOrSizing);
     UICharacterSheet.title = UICharacterSheet:CreateFontString(nil, "OVERLAY", "GameFontHighlight");
     UICharacterSheet.title:SetPoint("CENTER", UICharacterSheet.TitleBg, "CENTER", 6, 1);
     UICharacterSheet.title:SetText("Anathem Character Sheet");
@@ -84,17 +134,28 @@ function Config:CreateMenu()
     UICharacterSheet.ScrollFrame.ScrollBar:SetPoint("TOPLEFT", UICharacterSheet.ScrollFrame, "TOPRIGHT", -12, -18);
     UICharacterSheet.ScrollFrame.ScrollBar:SetPoint("BOTTOMRIGHT", UICharacterSheet.ScrollFrame, "BOTTOMRIGHT", -7, 18);
 
-    local child = CreateFrame("Frame", nil, UICharacterSheet.ScrollFrame);
-    child:SetSize(520, 540);
-    UICharacterSheet.ScrollFrame:SetScrollChild(child);
+    local content1, content2, content3 = SetTabs(UICharacterSheet, 3, "Statistiques", "Compétences", "Configuration");
     
     
+    ----------------------------------
+    -- content 1
+    ----------------------------------
+
     ----------------------------------
 	-- Buttons
     ----------------------------------
     
-    UICharacterSheet.saveButton = self:CreateButton("CENTER", child, "BOTTOM", 0, "Save");
+    content1.saveButton = self:CreateButton("CENTER", content1, "BOTTOM", 0, "Save");
 
+
+    ----------------------------------
+    -- content 2
+    ----------------------------------
+
+
+    ----------------------------------
+    -- content 3
+    ----------------------------------
 
     UICharacterSheet:Hide();
     return UICharacterSheet;
